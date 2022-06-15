cce445db00141b9b6c208e9afd1df631082c68f9
andre.gabry <andre.gabry@icloud.com>
2022-06-15 17:13:52 -0300

    Estruturando

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.png">
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Network</title>
+</head>
+
+<body>
+
+<div class="app">
+    <header class="app_header">
+        <div class="app_header_brand">
+            <img class="app_header_brand_logo" src="assets/img/brand.png">
+            <h1 class="app_header_brand_title">Network</h1>
+            <span class="app_header_icon material-symbols-outlined">menu</span>
+        </div>
+    </header>
+    <main class="app_main">
+        <div class="app_sidebar">
+            <div class="app_sidebar_profile">
+                <img src="assets/img/profile.JPG">
+                <h2>André Gabry</h2>
+                <p>andre.gabry@adventistas.org</p>
+            </div>
+            <div class="app_sidebar_content">
+
+                <a href="#">Usuários</a>
+                <a href="#">Equipamentos</a>
+                <a href="#">Licenças</a>
+                <a href="#">Sistemas</a>
+                <a href="#">Softwares</a>
+                <a href="#">Impressoras</a>
+
+            </div>
+            <div class="app_sidebar_footer">
+
+            </div>
+        </div>
+
+    </main>
+    <footer class="app_footer">
+        <p class="app_ass">Desenvilvido por Adré Gabry v1.0</p>
+    </footer>
+</div>
+
+</body>
+
+</html>
